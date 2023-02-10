892469c7fff2f80f2a6e2180a8851f104ce36830
piorkowski05 <125016528+piorkowski05@users.noreply.github.com>
2023-02-10 10:20:57 +0100

    Add files via upload

---
+++ program.js
@@ -0,0 +1,30 @@
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+
+// initialize kaboom context
+kaboom();
+
+loadSprite("kartka","kartka.png")
+loadSprite("gwiazda","gwiazda.png")
+loadSound("muzyka","Cichanoc.mp3")
+
+add([
+    sprite("kartka"),
+    pos(0,0)
+])
+
+const gwiazda = add([
+    sprite("gwiazda"),
+    pos(0,115)
+])
+
+add([
+    play("muzyka")
+])
+
+onUpdate(()=>{
+    if(gwiazda.pos.x<600) gwiazda.pos.x ++
+})
+
+// keyRelease("space", ()=>{
+//     play("muzyka")
+// })
